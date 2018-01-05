61f6fec8f4e5a2b2be37a55df178a07c94e5c1b1
Nick Ustinov <nickustinov@gmail.com>
2018-01-05 11:53:09 +0200

    - sha256 fix

--- Formula/lokalise.rb
+++ Formula/lokalise.rb
@@ -3,7 +3,7 @@ class Lokalise < Formula
   homepage "http://docs.lokalise.co/article/44l4f1hiZM-lokalise-cli-tool"
 
   version "0.44"
-  sha256 "22628b4c5be947890d403c5910e6c1a8dccc2c6bc290c6cc4c92aed9b2413210"
+  sha256 "78b98a1a4964c205aac22caf62791d6c1e3b79413a1784c5d295475285a41715"
 
   url "https://s3-eu-west-1.amazonaws.com/lokalise-assets/cli/lokalise-0.44-darwin-amd64.tgz"
 
