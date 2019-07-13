8a9c010cf2638e2edf929dbe4bf0e441d8b241c1
Nick Ustinov <nickustinov@gmail.com>
2019-07-13 10:17:37 +0300

    0.711

--- Formula/lokalise.rb
+++ Formula/lokalise.rb
@@ -2,10 +2,10 @@ class Lokalise < Formula
   desc "Lokalise CLI Tool - API client binary"
   homepage "https://docs.lokalise.co/api-and-cli/lokalise-cli-tool"
 
-  version "0.71"
-  sha256 "53e112c7947ef78ebba7c3f33bf5406430d9494aece46d04f4a13b2359315083"
+  version "0.711"
+  sha256 "dc373de606c45be4c928412a3c832b471dcd0268b3748da807b160c5644589b0"
 
-  url "https://s3-eu-west-1.amazonaws.com/lokalise-assets/cli/lokalise-0.71-darwin-amd64.tgz"
+  url "https://s3-eu-west-1.amazonaws.com/lokalise-assets/cli/lokalise-0.711-darwin-amd64.tgz"
 
   def install
     bin.install "lokalise"
